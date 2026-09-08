aa1a58c4971f5bec35cb06b7772c9d33a7820e85
AdityaZulkarnaen <adityazulkarnaen083@gmail.com>
2026-09-08 17:29:59 +0700

    fix: failed ci because gitignore publicbuild

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,5 +6,11 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        // Aset Vite tidak ikut di-commit, jadi tidak dibutuhkan saat menguji Blade.
+        $this->withoutVite();
+    }
 }
